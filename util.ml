c3f9b91788856b69a30e38d5dcbcad9fe078fac9
Geoff Hulette <ghulette@gmail.com>
2015-02-22 22:00:51 -0800

    Update

---
+++ util.ml
@@ -0,0 +1,20 @@
+let rec all_valuations ks vs =
+  let (>>=) m f = List.flatten (List.map f m) in
+  let pure x = [x] in
+  match ks with
+  | [] -> pure []
+  | k::ks' ->
+     List.map (fun v -> (k,v)) vs >>= fun kv ->
+     all_valuations ks' vs >>= fun vl ->
+     pure (kv :: vl)
+
+let time f x =
+    let t0 = Sys.time () in
+    let fx = f x in
+    let t1 = Sys.time () in
+    Printf.printf "Execution time: %fs\n" (t1 -. t0);
+    fx
+
+let rec seq = function
+  | 0 -> []
+  | n -> n :: seq (n-1)
